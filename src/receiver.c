02b3e31f6991fd7baacee2a78fffc25af2206c90
Lakshya013 <Lakshya5@students.ubc.ca>
2024-02-19 23:31:30 -0800

    push lakshya

--- src/receiver.c
+++ src/receiver.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
+#include <netdb.h> // included for getaddrinfo
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -11,10 +11,89 @@
 #include <pthread.h>
 #include <errno.h>
 
+#define MYPORT "4950"    // the port users will be connecting to
+#define HOSTNAME "127.0.0.1"
+#define MAXBUFLEN 100
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 void rrecv(unsigned short int myUDPport, 
             char* destinationFile, 
             unsigned long long int writeRate) {
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
 
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    char UDPport[6];
+    sprintf(UDPport, "%d", myUDPport);
+    if ((rv = getaddrinfo(HOSTNAME, UDPport, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return;
+    }
+
+    freeaddrinfo(servinfo);
+    printf("listener: waiting to recvfrom...\n");
+
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    printf("listener: got packet from %s\n",
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+    printf("listener: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    //sending reply
+    if ((numbytes = sendto(sockfd, "Recived Boss!", 13, 0,
+             (struct sockaddr *)&their_addr, addr_len)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+    close(sockfd);
+    return;
 }
 
 int main(int argc, char** argv) {
@@ -31,4 +110,6 @@ int main(int argc, char** argv) {
     }
 
     udpPort = (unsigned short int) atoi(argv[1]);
+    while(1) rrecv(udpPort, argv[2], 0);
+    return 0; 
 }
